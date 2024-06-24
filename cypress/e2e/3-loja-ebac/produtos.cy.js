e11eb9b6900d9fcd9f76e2c4bbb080f6e822df75
Alan P <alanpereira060@gmail.com>
2024-06-24 19:35:02 -0300

    Subindo modificaçõaes

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe('Funcionadlidade: produtos', () => {
+   
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+   
+   
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+        //.first()
+        //.last()
+        //.eq(2)
+        .contains('Atlas Fitness Tank')
+        .click()
+        cy.get('.sku_wrapper').should('contain','STMT11')
+    });
+});
